170edf0a634b52a830b4991e7f11093a81d06151
Francisco Cavalcanti <franciscoreis85@hotmail.com>
2022-05-25 15:08:21 -0300

    Added models, controllers, views, forms and some scss to be edited later

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,5 +7,6 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import "channels"
 
+
 Rails.start()
 Turbolinks.start()
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+Restaurant.create([{name: "China in Box", address: "R. Vicente da Fontoura, 1.830", phone_number: "(51) 98765-4321", category:"Chinese"},
+{name: "Spoleto", address: "R. João Wallig 1302", phone_number: "(51) 98765-4321", category:"Italian"},
+{name: "Shark Sushi", address: "Av. Ipiranga 4612", phone_number: "(51) 98765-4321", category:"Japanese"},
+{name: "Sushito", address: "Av. Teixeira Mendes 1161", phone_number: "(51) 98765-4321", category:"Japanese"},
+{name: "Via Morano", address: "Av. Ijuí 372", phone_number: "(51) 98765-4321", category:"Italian"},
+])
+
+p "Created #{Restaurant.count} restaurants"
+# ["chinese", "italian", "japanese", "french", "belgian"].
